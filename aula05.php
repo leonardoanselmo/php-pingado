7293e600bb574ae74e6be04b9a10d2161e68928e
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-26 18:45:47 -0300

    source aula05.php

---
+++ aula05.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Operadores Aritméticos</title>
+</head>
+<body>
+	<?php 
+		$numero1 = 5;
+		$numero2 = 6;
+		$soma = $numero1 + $numero2;
+		echo "A soma é: $soma";
+	?>
+	
+</body>
+</html>
