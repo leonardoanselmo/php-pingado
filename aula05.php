16d626c95d1a3ff53906b1e86f531a3f4e3032f3
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-26 18:49:20 -0300

    source aula05.php

--- aula05.php
+++ aula05.php
@@ -9,7 +9,9 @@
 		$numero1 = 5;
 		$numero2 = 6;
 		$soma = $numero1 + $numero2;
-		echo "A soma é: $soma";
+		echo "A soma é: $soma"."<br/>";
+		echo "A soma é: ".($numero1 + $numero2);
+
 	?>
 	
 </body>
