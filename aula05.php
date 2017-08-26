0820dcc16fd315cc4cb5b905b0d09c12326ba03d
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-26 18:53:04 -0300

    source aula05.php

--- aula05.php
+++ aula05.php
@@ -10,7 +10,11 @@
 		$numero2 = 6;
 		$soma = $numero1 + $numero2;
 		echo "A soma é: $soma"."<br/>";
-		echo "A soma é: ".($numero1 + $numero2);
+		echo "A soma é: ".($numero1 + $numero2)."<br/>";
+		echo "A subtração é: ".($numero1 - $numero2)."<br/>";
+		echo "A Multiplicação é: ".($numero1 * $numero2)."<br/>";
+		echo "A divisão é: ".($numero1 / $numero2)."<br/>";
+		echo "O módulo é: ".($numero1 % $numero2)."<br/>";
 
 	?>
 	
