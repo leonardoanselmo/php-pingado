ed6aeef3b1f67cf456be94cb21bc9f414f3c03d4
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-26 18:30:37 -0300

    source aula04

---
+++ aula04.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Variáveis</title>
+</head>
+<body>
+
+	<?php 
+		$numero = 4;
+		$nome = 'Leonardo';
+		print $nome;
+	?>
+
+	
+</body>
+</html>
+
