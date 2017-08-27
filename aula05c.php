c2bde9ac7e9c507db043ff14ad7d4975d23aaf36
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-27 14:34:53 -0300

    source aula05c

--- aula05c.php
+++ aula05c.php
@@ -17,8 +17,10 @@
 	<?php 
 		$v1 = $_GET['a'];
 		$v2 = $_GET['b'];
-		echo "<h2> Valores recebidos de: $v1 e $v2</h2>";
-		echo "O valor absoluto de $v2 é: ".abs($v2);
+		echo "<h2> Valores recebidos são: $v1 e $v2</h2>";
+		echo "O valor absoluto de $v2 é: ".abs($v2)."</br>";
+		echo "O valor de $v1<sup>$v2</sup> é: ".pow($v1, $v2)."</br>";
+		echo "A raiz quadrada de $v1 é: ".sqrt($v1);
 
 	?>
 	
